c9fbfa457def73f8504508d28ccb1368de9fe3ca
Abuzar <abuzarkhan1242@gmail.com>
2025-04-16 12:36:15 +0500

    Spring boot project _1

---
+++ src/test/java/com/abuzar/FirstProject/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abuzar.FirstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
